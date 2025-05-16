608aef89f8e1f6d0a1bcecbc3817cd30d00c45ac
Juan-Restrepo-Dev <juanrestrepodev16@gmail.com>
2025-05-15 19:43:27 -0500

    Update create_cv.py

---
+++ cv/create_cv.py
@@ -0,0 +1,9 @@
+import json
+
+
+def add_cv_to(cv):
+    with open('db.json', 'r') as f:
+       db = json.load(f)
+       db["cvs"].append(cv)
+    with open('db.json', 'w') as f:
+       json.dump(db, f, indent=4)  
