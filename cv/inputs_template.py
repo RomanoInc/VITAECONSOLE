139376d1bdbe66390b3a6bc5fa135c99bcafca35
Juan-Restrepo-Dev <juanrestrepodev16@gmail.com>
2025-05-16 20:30:58 -0500

    Update inputs_template.py

--- cv/inputs_template.py
+++ cv/inputs_template.py
@@ -1,4 +1,3 @@
-
 from textual.screen import Screen
 from textual import events
 from textual.app import App, ComposeResult
@@ -27,13 +26,12 @@ from textual.widgets import (
 
 )
 WIDGETS_MD = """\
-# Widgets
-
-The Textual library includes a large number of builtin widgets.
-
-The following list is *not* exhaustive…
+# Informacion hoja de vida
  
 """
+
+
+INPUT_DATA = []
 
 # class InputApp(App):
 #     """App to display key events."""
@@ -45,12 +43,16 @@ The following list is *not* exhaustive…
 #         self.query_one(RichLog).write(event)
 
 
-class Inputs(containers.VerticalGroup , title = "", label = []):
+class Inputs(containers.VerticalGroup ):
+    def __init__(self, title = "", labels = []):
+        super().__init__(self, title = "", labels = [])
+        # Inicializar los atributos de la clase
+        self.title = title
+        self.labels = labels
 
     ALLOW_MAXIMIZE = True
     DEFAULT_CLASSES = "column"
-    INPUTS_MD = """\## {title}"""
-    LABELS = label
+    INPUTS_MD = """\## hola """
     DEFAULT_CSS = """
     Inputs {
         Grid {
@@ -76,26 +78,30 @@ class Inputs(containers.VerticalGroup , title = "", label = []):
     def compose(self) -> ComposeResult:
         yield Markdown(self.INPUTS_MD)
         with containers.Grid():
-            for i in labels:
-                yield Label(f"{i}")
+            for i in self.labels:
+                lbl = yield Label(f"{i}")
+                inp = yield Input()
 
-            yield Label("name")
-            yield Input(placeholder="Type anything here")
-            yield Label("id")
-            yield Input(
-                type="number", placeholder="Type a number here", valid_empty=True
-            )
-            yield Label("email")
-            r = yield MaskedInput(
-                "9999-9999-9999-9999;0",
-                tooltip="Obviously not your real credit card!",
-                valid_empty=True,
-            )
-            yield Label("Country")
-            yield Input(
-                suggester=SuggestFromList(COUNTRIES, case_sensitive=False),
-                placeholder="Country",
-            )
+                INPUT_DATA.append(lbl)
+
+
+            # yield Label("name")
+            # yield Input(placeholder="Type anything here")
+            # yield Label("id")
+            # yield Input(
+            #     type="number", placeholder="Type a number here", valid_empty=True
+            # )
+            # yield Label("email")
+            # r = yield MaskedInput(
+            #     "9999-9999-9999-9999;0",
+            #     tooltip="Obviously not your real credit card!",
+            #     valid_empty=True,
+            # )
+            # yield Label("Country")
+            # yield Input(
+            #     suggester=SuggestFromList(COUNTRIES, case_sensitive=False),
+            #     placeholder="Country",
+            # )
 
 class WidgetsScreen(PageScreen):
     """The Widgets screen"""
@@ -115,14 +121,16 @@ class WidgetsScreen(PageScreen):
     """
 
     BINDINGS = [Binding("escape", "blur", "Unfocus any focused widget", show=False)]
-
+    
     def compose(self) -> ComposeResult:
         with lazy.Reveal(containers.VerticalScroll(can_focus=True)):
+            loquesea = ["name","id","email","tel","dir"]
             yield Markdown(WIDGETS_MD, classes="column")
             # yield Buttons()
             # yield Checkboxes()
             # yield Datatables()
-            yield Inputs()
+            objet = Inputs("hola",loquesea)
+            yield objet
             # yield ListViews()
             # yield Logs()
             # yield Markdowns()
@@ -142,7 +150,11 @@ if __name__ == "__main__":
     class cv_inputs(App):
         def get_default_screen(self) -> Screen: # type: ignore
             return WidgetsScreen()
+        # def run_input_data():
+        #     return INPUT_DATA
 
+    # data = cv_inputs.run_input_data()
+    # print(data)
     app = cv_inputs()
     app.run()
 
